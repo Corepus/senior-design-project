c90ef931e9085605b325428e8fe653955fed34ae
Ryuto Kitagawa <ryu51920@gmail.com>
2019-03-30 15:17:41 -1000

    UIDesign Started

---
+++ iRoutePlanner/iRoutePlanner/TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewController.swift
+//  iRoutePlanner
+//
+//  Created by Guest Code User on 3/30/19.
+//  Copyright © 2019 BMW Fire. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- iRoutePlanner/iRoutePlanner/ViewController.swift
+++ iRoutePlanner/iRoutePlanner/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var input: UITextField!
+    
+    var userInput: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        input.delegate = self
+        configureTapGesture()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    private func configureTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap() {
+        input.resignFirstResponder()
+    }
+    
 
-
+    @IBAction func addButton(_ sender: Any) {
+        addToArray()
+    }
+    
+    @IBAction func click(_ sender: Any) {
+        print("Click Click Mother Fucker")
+    }
+    
+    func addToArray() {
+        guard let add = input.text else {return}
+        input.text = ""
+        userInput.append(add)
+        print(userInput)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addToArray()
+        return true
+    }
 }
-
